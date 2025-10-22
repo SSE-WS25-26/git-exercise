dfd3e7a13be3fee049cc6d133c52ef1e75ed0efe
David Enoghama <61471574+the-mr-dave@users.noreply.github.com>
2025-10-22 17:02:40 +0200

    Tested substraction in exercise 1 (#1)

--- exercise1.py
+++ exercise1.py
@@ -46,4 +46,8 @@ def test_3d_vector_addition() -> None:
 
 def test_3d_vector_subtraction() -> None:
     # Task B: add a test for vector subtraction
-    assert False
+    v = Vector([0.0, 0.0, 0.0]).__sub__(Vector([1.0, 2.0, 3.0]))
+    assert isclose(v[0], -1.0)
+    assert isclose(v[1], -2.0)
+    assert isclose(v[2], -3.0)
+    
