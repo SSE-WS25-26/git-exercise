98517f04e62b90d7aa7df1083b845536b4ec82ba
AlinaChristin <189384806+AlinaChristin@users.noreply.github.com>
2025-10-26 18:12:42 +0100

    Implement method to check if a value is within an interval (#6)
    * Implement method to check if a value is within an interval
    
    * Change type from int to float in is_in_intervall method
    
    ---------
    
    Co-authored-by: David Enoghama <61471574+the-mr-dave@users.noreply.github.com>

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,7 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x 
         ll_py = point.y - self._lower_left.y
-        return ll_px >= -tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= -tolerance and ll_py <= self._dy + tolerance
+        return self.is_in_interval(ll_px, 0, self._dx, tolerance) and self.is_in_interval(ll_py,0,self._dy, tolerance)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,7 +40,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
+    def is_in_interval(self, value: float, min_value: float, max_value: float, tolerance: float) -> bool: # Task A
+        return min_value - tolerance <= value <= max_value + tolerance
 
 
 def test_rectangle_contains_exact() -> None:
